bf83c4dca92fc82c7a5584f0b07735173a7fe181
fastlorenzo <git@bernardi.be>
2022-10-02 13:40:35 +0200

    Updated entrypoint script
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- apps/nextcloud-client/entrypoint.sh
+++ apps/nextcloud-client/entrypoint.sh
@@ -4,6 +4,26 @@
 PUID=${PUID:-911}
 PGID=${PGID:-911}
 
+# Default settings
+NC_EXIT=${NC_EXIT:=true}
+NC_OPTIONS="--non-interactive"
+CHOWN_FILES=${CHOWN_FILEs:=false}
+
+# Append -h to NC_OPTIONS if NC_HIDDEN is true
+if [ "$NC_HIDDEN" = "true" ]; then
+    NC_OPTIONS="$NC_OPTIONS -h"
+fi
+
+# Append --silent to NC_OPTIONS if NC_SILENT is true
+if [ "$NC_SILENT" = "true" ]; then
+    NC_OPTIONS="$NC_OPTIONS --silent"
+fi
+
+# Append --trust if NC_TRUST is true
+if [ "$NC_TRUST" = "true" ]; then
+    NC_OPTIONS="$NC_OPTIONS --trust"
+fi
+
 groupmod -o -g "$PGID" ncsync
 usermod -o -u "$PUID" ncsync
 
@@ -22,35 +42,70 @@ chown -R ncsync:ncsync /settings
 
 # check exclude file exists
 if [ -e "/settings/exclude" ]; then
-	EXCLUDE="--exclude /settings/exclude"
+	NC_OPTIONS="$NC_OPTIONS --exclude /settings/exclude"
 else
 	echo "INFO: exclude file not found!"
 fi
+
 # check unsyncedfolders file exists
 if [ -e "/settings/unsyncfolders" ]; then
-	UNSYNCEDFOLDERS="--unsyncedfolders /settings/unsyncfolders"
+	NC_OPTIONS="$NC_OPTIONS --unsyncedfolders /settings/unsyncfolders"
 else
 	echo "INFO: unsync file not found!"
 fi
 
+# Check if NC_USER is set, if not, exit
+if [ -z "$NC_USER" ]; then
+    echo "ERROR: NC_USER is not set!"
+    exit 1
+else
+    NC_OPTIONS="$NC_OPTIONS --user $NC_USER"
+fi
+# Check if NC_PASS is set, if not, exit
+if [ -z "$NC_PASS" ]; then
+    echo "ERROR: NC_PASS is not set!"
+    exit 1
+else
+    NC_OPTIONS="$NC_OPTIONS --password $NC_PASS"
+fi
+# Check if NC_PATH is set, if not, exit
+if [ -z "$NC_PATH" ]; then
+    echo "ERROR: NC_PATH is not set!"
+    exit 1
+else
+    NC_OPTIONS="$NC_OPTIONS --path $NC_PATH"
+fi
+
+# Check if NC_URL is set, if not, exit
+if [ -z "$NC_URL" ]; then
+    echo "ERROR: NC_URL is not set!"
+    exit 1
+fi
+
+# Check if NC_SOURCE_DIR is set, if not, exit
+if [ -z "$NC_SOURCE_DIR" ]; then
+    echo "ERROR: NC_SOURCE_DIR is not set!"
+    exit 1
+fi
+
 while true
 do
-	su-exec ncsync nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) $( [ "$EXCLUDE" ] && echo $EXCLUDE ) $( [ "$UNSYNCEDFOLDERS" ] && echo $UNSYNCEDFOLDERS ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL
+	su-exec ncsync nextcloudcmd $NC_OPTIONS $NC_SOURCE_DIR $NC_URL
 
-	#chown the files to the USER_UID:
-	echo "chown -R ncsync:ncsync $NC_SOURCE_DIR";
-	chown -R ncsync:ncsync $NC_SOURCE_DIR
+	# chown the files to the USER_UID if CHOWN_FILES is true
+    if [ "$CHOWN_FILES" = "true" ]; then
+        echo "INFO: chowning files in $NC_SOURCE_DIR to $PUID:$PGID"
+        chown -R ncsync:ncsync $NC_SOURCE_DIR
+    fi
 
-	#check if exit!
-	if [ "$NC_EXIT" = true ] ; then
+	# Exit 0 if NC_EXIT is true
+    if [ "$NC_EXIT" = "true" ]; then
+        echo "INFO: NC_EXIT is true, exiting..."
+        exit 0
+    fi
 
-		if [  ! "$NC_SILENT" == true ] ; then
-			echo "NC_EXIT is true so exiting... bye!"
-		fi
-
-		exit;
-	fi
-	sleep $NC_INTERVAL
+    echo "INFO: NC_EXIT is false, sleeping for $NC_INTERVAL seconds..."
+    sleep $NC_INTERVAL
 done
 
 exit 1
