2537f20950ddf0fae4cec03b472760b2e9f79274
fastlorenzo <git@bernardi.be>
2023-04-12 20:20:52 +0200

    Updated apps to build
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- apps/sonarr/ci/latest.sh
+++ apps/sonarr/ci/latest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 channel=$1
 
-if [[ "${channel}" == "widowmaker" ]]; then
-    version=$(curl -sX GET "https://sonarr.servarr.com/v1/update/${channel}/changes?os=linux&runtime=netcore" | jq --raw-output '.[0].version')
+if [[ "${channel}" == "develop" ]]; then
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version' 2>/dev/null)
 else
-    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=3" | jq --raw-output '.version')
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=3" | jq --raw-output '.version' 2>/dev/null)
 fi
 
 version="${version#*v}"
