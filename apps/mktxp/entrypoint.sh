f8f52c26ed6bdccdb4d4e8d65d784c3ca1fb3ce9
fastlorenzo <git@bernardi.be>
2023-04-13 08:52:49 +0200

    Fixed containers
    Signed-off-by: fastlorenzo <git@bernardi.be>

---
+++ apps/mktxp/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#shellcheck disable=SC1091
+test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
+test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
+
+#shellcheck disable=SC2086
+exec /app/mktxp "$@"
