a24f2acc7ecf15cc1646ce2a559551bc02285088
fastlorenzo <git@bernardi.be>
2024-12-22 00:28:53 +0100

    synced repo
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- apps/bazarr/entrypoint.sh
+++ apps/bazarr/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-
-mkdir -p /config
-
 #shellcheck disable=SC2086
+
 exec \
-    /usr/bin/python3 \
-        /app/bazarr.py \
+    /usr/local/bin/python \
+        /app/bin/bazarr.py \
             --no-update \
             --config /config \
-            --port ${BAZARR__PORT:-6767} \
+            --port ${BAZARR__PORT} \
             "$@"
