88075d152d2f38bebdd94709983dd741ebcfcb2e
fastlorenzo <git@bernardi.be>
2024-12-22 00:54:14 +0100

    fix latest
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- apps/mktxp/ci/latest.sh
+++ apps/mktxp/ci/latest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-# No version information from https://github.com/akpw/mktxp
-version=$(curl -sXGET https://pypi.org/project/mktxp/ | grep '<h1 class="package-header__name">' -A1 | tail -n1 | awk '{print $2}')
+version=$(curl -sX GET "https://api.github.com/repos/akpw/mktxp/releases/latest" | jq --raw-output '.tag_name')
+version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
